72d4e59e49a9b41af71405ec360ba1f3beab0091
Replit user <>
2023-08-08 10:23:19 +0000

    initial commmit

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
-from flask import Flask, jsonify, render_template, request
-from database import load_jobs_from_db, load_job_from_db 
-# add_application_to_db
+from flask import Flask, render_template
+import requests
+import os
+my_secret = os.environ['api_key']
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_jovian():
-  jobs = load_jobs_from_db()
-  return render_template('index.html', 
-                         jobs=jobs)
-@app.route("/job/<id>")
-def show_jobs(id):
-    job = load_job_from_db(id)
-    if job is None:
-        return jsonify({})
-    else:
-        return render_template("job_pages.html",jobs=job)
+    return render_template('index.html')
 
-@app.route("/job/<id>/apply", methods=['post'])
-def apply_jobs(id):
-  data = request.form
-  job = load_job_from_db(id)
-  # add_application_to_db(id, data)
-  return render_template('application_submitted.html', 
-                         application=data,
-                         jobs=job)
-@app.route("/about")
-def about():
-  return render_template("about.html")
-  
+@app.route('/movie/<id>')
+def movie_details(id):
+    url = f'https://api.themoviedb.org/3/movie/{id}?api_key={my_secret}'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        movie_data = response.json()
+        return render_template('movie_details.html', movie_data=movie_data)
+    else:
+        return 'Error fetching data from TMDb API.'
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
+
+  
